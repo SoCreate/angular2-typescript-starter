3a55d73af45ee0ce9f4271bcb8f43de2ad648c43
Justin Schwartzenberger <schwarty@gmail.com>
2015-07-09 23:14:05 -0700

    Removed angular packages in favor of remote scripts for now. Added typescript tsd and compile tasks. Added angular component.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,42 @@
 var gulp = require('gulp');
 var gls = require('gulp-live-server');
 var del = require('del');
+var tsd = require('gulp-tsd');
+var tsc = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
 var clientDependencies = require('./clientdependencies.json');
+
+gulp.task('get-tsds', function (callback) {
+    tsd({
+        command: 'reinstall',
+        config: './tsd.json'
+    }, callback);
+});
 
 gulp.task('client-dependencies', function (callback) {
 	var dest = 'src/client-dependencies'
 	del.sync([dest]);
 	var sources = [];
 	var sourceRootPath = './' + clientDependencies.rootPath + '/';
-	clientDependencies.dependencies.forEach(function(dependency) {
+	clientDependencies.dependencies.forEach(function (dependency) {
 		sources.push(sourceRootPath + dependency);
 	});
-	gulp.src(sources, {base: clientDependencies.rootPath})
+	gulp.src(sources, { base: clientDependencies.rootPath })
 		.pipe(gulp.dest(dest));
 });
 
-gulp.task('serve', function() {
+gulp.task('compile-ts', function () {
+	var tsProject = tsc.createProject('tsconfig.json', {
+		typescript: require('typescript')
+	});
+	var result = gulp.src('./src/**/*ts')
+		.pipe(tsc(tsProject));
+
+	return result.js
+		.pipe(gulp.dest('src/'));
+});
+
+gulp.task('serve', function () {
     var server = gls.static('src', 8000);
     server.start();
     gulp.watch(['src/**/*.js', 'src/**/*.css', 'src/**/*.html'], function () {
@@ -23,4 +44,4 @@ gulp.task('serve', function() {
     });
 });
 
-gulp.task('default', ['client-dependencies'])
+gulp.task('default', ['get-tsds', 'client-dependencies', 'compile-ts'])
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap} from 'angular2/angular2';
+@Component({
+  selector: 'app'
+})
+@View({
+  template: 'Angular2-Typescript-Starter'
+})
+class AppComponent {
+}
+bootstrap(AppComponent);
