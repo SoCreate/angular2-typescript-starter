7dd5b18bc8941d143678ddd953a2fdfd3e13dc8c
schwarty <schwarty@gmail.com>
2015-07-09 17:45:18 -0700

    added client dependency build and live server

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
+var gls = require('gulp-live-server');
+var del = require('del');
 var clientDependencies = require('./clientdependencies.json');
 
 gulp.task('client-dependencies', function (callback) {
+	var dest = 'src/client-dependencies'
+	del.sync([dest]);
 	var sources = [];
 	var sourceRootPath = './' + clientDependencies.rootPath + '/';
 	clientDependencies.dependencies.forEach(function(dependency) {
 		sources.push(sourceRootPath + dependency);
 	});
 	gulp.src(sources, {base: clientDependencies.rootPath})
-		.pipe(gulp.dest('src/lib'));
+		.pipe(gulp.dest(dest));
+});
+
+gulp.task('serve', function() {
+    var server = gls.static('src', 8000);
+    server.start();
+    gulp.watch(['src/**/*.js', 'src/**/*.css', 'src/**/*.html'], function () {
+        server.notify.apply(server, arguments);
+    });
 });
 
 gulp.task('default', ['client-dependencies'])
