8514109db86ed4d2c7da5a82945bd58aeb1af0a4
Richard McCluskey <rjmccluskey@gmail.com>
2015-07-13 18:22:22 -0700

    added routerInjectables into angular bootstrap, this closes #4

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 /// <reference path="../typings/angular2/angular2.d.ts" />
+/// <reference path="../typings/angular2/router.d.ts" />
 import {Component, View, bootstrap} from 'angular2/angular2';
+import {routerInjectables} from 'angular2/router';
 @Component({
   selector: 'app'
 })
@@ -8,4 +10,4 @@ import {Component, View, bootstrap} from 'angular2/angular2';
 })
 class AppComponent {
 }
-bootstrap(AppComponent);
+bootstrap(AppComponent, [routerInjectables]);
