582d27259d63133874601e21d94bd139336c1625
Justin Schwartzenberger <schwarty@gmail.com>
2015-07-15 23:59:26 -0700

    Added http injectables and sample code for using http.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 /// <reference path="../typings/tsd.d.ts" />
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component, View, bootstrap, Inject} from 'angular2/angular2';
+import {httpInjectables, Http} from 'angular2/angular2';
 import {routerInjectables} from 'angular2/router';
+
 @Component({
   selector: 'app'
 })
@@ -8,5 +10,10 @@ import {routerInjectables} from 'angular2/router';
   template: 'Angular2-Typescript-Starter'
 })
 class AppComponent {
+  constructor( @Inject(Http) http: Http) {
+    http.get('people.json').observer({
+      next: (value) => console.log(value.json())
+    });
+  }
 }
-bootstrap(AppComponent, [routerInjectables]);
+bootstrap(AppComponent, [routerInjectables, httpInjectables]);
